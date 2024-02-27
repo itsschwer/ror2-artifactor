f16e1a1822adafd35dced9063b05b8fbb5d64325
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-02-27 11:42:27 +1100

    Improve Voracity icon

--- src/Content/Voracity.cs
+++ src/Content/Voracity.cs
@@ -18,23 +18,11 @@ namespace Artifactor
         protected readonly Sprite disabledIcon;
 
         public Voracity() : base() {
-            Sprite src = Addressables.LoadAssetAsync<Sprite>("RoR2/DLC1/BearVoid/texBuffBearVoidCooldown.png").WaitForCompletion();
-            
-            enabledIcon = CreateIcon(src, Color.white);
-            disabledIcon = CreateIcon(src, Color.gray);
-        }
+            Sprite src = Addressables.LoadAssetAsync<Sprite>("RoR2/DLC1/BearVoid/texBuffBearVoidReady.tif").WaitForCompletion();
+            enabledIcon = CreateIcon(src, new Color(231/255f, 199/255f, 231/255f));
+            disabledIcon = CreateIcon(src, new Color(71/255f, 62/255f, 69/255f));
 
-        private static Sprite CreateIcon(Sprite src, Color tint)
-        {
-            Texture2D tex = Object.Instantiate(src.texture);
-            Color[] cols = tex.GetPixels();
-            for (int i = 0; i < cols.Length; i++) {
-                cols[i].r *= tint.r;
-                cols[i].g *= tint.g;
-                cols[i].b *= tint.b;
-            }
-
-            return Sprite.Create(tex, src.rect, src.pivot, src.pixelsPerUnit);
+            Init();
         }
 
         private bool IsHost => UnityEngine.Networking.NetworkServer.active;
@@ -84,5 +72,35 @@ namespace Artifactor
         }
 
         private static readonly Dictionary<TeamIndex, int> softTeamCharacterLimits = [];
+
+
+
+
+        private static Sprite CreateIcon(Sprite src, Color tint)
+        {
+            Texture2D tex = DuplicateTexture(src.texture);
+            Color[] cols = tex.GetPixels();
+            for (int i = 0; i < cols.Length; i++)
+            {
+                cols[i].r *= tint.r;
+                cols[i].g *= tint.g;
+                cols[i].b *= tint.b;
+            }
+            tex.SetPixels(cols);
+            tex.Apply(true, true);
+            return Sprite.Create(tex, src.rect, src.pivot, src.pixelsPerUnit);
+        }
+
+        // Reference: https://stackoverflow.com/questions/44733841/how-to-make-texture2d-readable-via-script/44734346#44734346
+        private static Texture2D DuplicateTexture(Texture2D src)
+        {
+            RenderTexture rt = RenderTexture.GetTemporary(src.width, src.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+            Graphics.Blit(src, rt);
+            RenderTexture.active = rt;
+            Texture2D tex = new Texture2D(src.width, src.height);
+            tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+            tex.Apply();
+            return tex;
+        }
     }
 }
