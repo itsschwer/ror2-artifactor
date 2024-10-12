654f372b189e7a723dab426e8c51f5e8f58571e6
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-12 13:23:48 +1100

    Restore Artifact of Voracity

--- src/Content.cs
+++ src/Content.cs
@@ -7,12 +7,14 @@ namespace Artifactor
 #if DISCONINUITY
         public static ArtifactDef Discontinuity { get; private set; }
 #endif
+        public static ArtifactDef Voracity;
 
         internal static void Init()
         {
 #if DISCONINUITY
             Discontinuity = new Discontinuity().artifactDef;
 #endif
+            Voracity = new Voracity().artifactDef;
 
             Plugin.Logger.LogMessage("~content initialized");
         }
